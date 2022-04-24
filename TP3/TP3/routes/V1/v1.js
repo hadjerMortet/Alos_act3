9deebdd2f917731138e46387e70ab7db3618116e
hadjerMortet <72406358+hadjerMortet@users.noreply.github.com>
2022-04-25 00:49:39 +0100

    Add files via upload

---
+++ TP3/TP3/routes/V1/v1.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const joi = require("joi");
+const route = express.Router();
+const Restaurants = require("../Restaurants.json");
+const validatee = require("../routes/middlewares/validate");
+
+route.get("/", (req, res) => {
+  res.json(Restaurants);
+});
+route.get("/:id", (req, res) => {
+  const index = Restaurants.find((e) => e.id === req.params.id);
+  index ? res.status(200).json(index) : res.status(404).send("NOT FOUND");
+});
+route.post("/", validatee, (req, res) => {
+  const id = Restaurants[Restaurants.length - 1].id + 1;
+  Restaurants.push({ id, ...req.body });
+  // throw Error("somthing faile"); //throw error to test error handling
+  res.status(200).send("added succefuly");
+});
+route.put("/:id", validatee, (req, res) => {
+  const index = Restaurants.find((e) => e.id === req.params.id);
+  Restaurants[index] = req.body;
+  res.status(200).send("Updated  succefuly");
+});
+route.delete("/:id", (req, res) => {
+  const newrestaurant = Restaurants.filter((e) => e.id != req.params.id);
+  Restaurants = newrestaurant;
+  res.status(200).send("deleted   succefuly");
+});
+
+module.exports = route;
---
+++ TP3/TP3/routes/V2/v2.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const joi = require("joi");
+const route = express.Router();
+const Restaurants = require("../Restaurants.json");
+const validatee = require("../routes/middlewares/validate");
+const UserController = require ("./Controllers/UserController")
+const ReviewController = require ("./Controllers/ReviewController")
+
+
+//  NEW FEATURE IN V2 :
+// reviews
+
+router.get('/Restaurants/:id/reviews',
+    ReviewController.get)
+
+router.post('/Restaurants/:id/reviews',
+    ReviewController.create)
+
+router.delete('reviews/:id',
+    ReviewController.delete)
+
+
+
+//  NEW FEATURE IN V2 :
+// users
+
+route.get('/users',
+    UserController.get_all)
+
+route.get('/users/:id',
+    UserController.get)
+
+route.get('/users/:id/reviews',
+    UserController.get_reviews)
+
+//  ############################
+route.get("/", (req, res) => {
+  res.json(Restaurants);
+});
+
+// GET DATA BY ID
+route.get("/:id", (req, res) => {
+  const index = Restaurants.find((e) => e.id === req.params.id);
+  index ? res.status(200).json(index) : res.status(404).send("NOT FOUND");
+});
+
+//  POST DATA TO RECIPES API AND CHECK WITH VALIDATEE MIDDLEWARE
+route.post("/", validatee, (req, res) => {
+  const id = Restaurants[Restaurants.length - 1].id + 1;
+  Countries.push({ id, ...req.body });
+  // throw Error("somthing faile"); //throw error to test error handling
+  res.status(200).send("added succefuly");
+});
+
+// MODIFY DATA IN API BY ID
+route.put("/:id", validatee, (req, res) => {
+  const index = Restaurants.find((e) => e.id === req.params.id);
+  Restaurants[index] = req.body;
+  res.status(200).send("Updated  succefuly");
+});
+
+//  DELETE DATA BY ID
+route.delete("/:id", (req, res) => {
+  const newrestaurant = Restaurants.filter((e) => e.id != req.params.id);
+	Restaurants= newrestaurant;
+  res.status(200).send("deleted   succefuly");
+});
+
+https: module.exports = route;
